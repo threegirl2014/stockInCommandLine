2986e2c3dbe9672dc26d31859ff5a51a8ecb0dbc
threegirl2014 <rujiazhang@foxmail.com>
2015-05-31 22:56:36 +0800

    增加功能：
    1. 显示当前时间
    
    2. 5s定时刷新
    
    3. mac osx的Terminal.app清屏。其他操作系统没有测试。

--- README.md
+++ README.md
@@ -54,3 +54,27 @@ xxx:
 105：粉底
 106：亮蓝底
 107：灰底
+
+====================================
+
+#2015-05-31
+
+mac osx下的文本操作键：http://my.oschina.net/kelvinline/blog/322671
+
+python 中获取当前时间：http://www.jb51.net/article/45657.htm
+
+python 获取股票信息：http://blog.chinaunix.net/uid-20196333-id-1972442.html
+
+python 定时刷新：http://blog.csdn.net/lxlzhn/article/details/8940125
+
+python 获取操作系统类型：http://www.cnblogs.com/itech/archive/2011/01/13/1934653.html
+
+python 执行命令行清屏：http://www.cnblogs.com/maybego/p/3234055.html
+
+增加功能：
+
+1. 显示当前时间
+
+2. 5s定时刷新
+
+3. mac osx的Terminal.app清屏。其他操作系统没有测试。
--- src/stockinCommand.py
+++ src/stockinCommand.py
@@ -7,7 +7,9 @@
 
 import urllib
 import string
-
+import time
+import os
+import platform
 from terminalColor import bcolors
 
 # from matplotlib.finance import stock_dt
@@ -30,6 +32,10 @@ sh600830,\
 sh601318,\
 sz150019"
 
+def getTime():
+    return time.strftime('%Y-%m-%d %A %p %X', time.localtime(time.time()))
+
+
 def name_in_mystock(name):
     i = 0
     for s in mystock:
@@ -48,42 +54,50 @@ data = urllib.urlopen(url).read().decode('gb2312')
 #print type(data)
 line = data.split('\n')
 
-print "代码      名称         昨收        今开     最高        最低       现价     涨幅"
+def printStock():
+    print bcolors.WHITE, "代码      名称         昨收        今开     最高        最低       现价     涨幅", bcolors.ENDC
 
-for stock in line:
-    stockInfo = stock.split(',')
-    if stockInfo[0]:
-        #var hq_str_s_sh000001="上证指数
-        temp  = stockInfo[0].split('_')[2].replace('"','').split('=')
-        code = temp[0]
-        name = temp[1]
-        todayBeginPrice   = string.atof(stockInfo[1])
-        yersterdayEndPrice  = string.atof(stockInfo[2])
-        currentPrice   = string.atof(stockInfo[3])
-        todayMaxPrice = string.atof(stockInfo[4])
-        todayMinPrice = string.atof(stockInfo[5])
-        if '%.2f' % todayBeginPrice == '0.00':
-            per = u'停牌'
-        else:
-            per = ( '%+.2f' % ( ( currentPrice / yersterdayEndPrice - 1 ) * 100 ) )+ '%'
-        
-        todayBeginPriceColor = highOrLow(todayBeginPrice, yersterdayEndPrice)
-        currentPriceColor = highOrLow(currentPrice, yersterdayEndPrice)
-        todayMaxPriceColor = highOrLow(todayMaxPrice, yersterdayEndPrice)
-        todayMinPriceColor = highOrLow(todayMinPrice, yersterdayEndPrice)
-#         index = name_in_mystock (name)
-#         if index :
-#             mypri = mystock[index-1][2]
-#             mypro = (cur - mypri) * mystock[index-1][1]
-#             print '%s %10.2f %10.2f %10.2f %12dw %10.3f %10.2f' %(name, cur, wave, per, money, mypri, mypro)
-        print '%s %4s %10.2f %s%10.2f%s %s%10.2f%s %s%10.2f%s %s%10.2f   %s%s' % \
-            (code, 
-             name, 
-             yersterdayEndPrice, 
-             todayBeginPriceColor, todayBeginPrice, bcolors.ENDC, 
-             todayMaxPriceColor, todayMaxPrice, bcolors.ENDC, 
-             todayBeginPriceColor, todayMinPrice, bcolors.ENDC, 
-             currentPriceColor, currentPrice, per, bcolors.ENDC)
+    for stock in line:
+        stockInfo = stock.split(',')
+        if stockInfo[0]:
+            #var hq_str_s_sh000001="上证指数
+            temp  = stockInfo[0].split('_')[2].replace('"','').split('=')
+            code = temp[0]
+            name = temp[1]
+            todayBeginPrice   = string.atof(stockInfo[1])
+            yersterdayEndPrice  = string.atof(stockInfo[2])
+            currentPrice   = string.atof(stockInfo[3])
+            todayMaxPrice = string.atof(stockInfo[4])
+            todayMinPrice = string.atof(stockInfo[5])
+            if '%.2f' % todayBeginPrice == '0.00':
+                per = u'停牌'
+            else:
+                per = ( '%+.2f' % ( ( currentPrice / yersterdayEndPrice - 1 ) * 100 ) )+ '%'
+            #红涨绿跌
+            todayBeginPriceColor = highOrLow(todayBeginPrice, yersterdayEndPrice)
+            currentPriceColor = highOrLow(currentPrice, yersterdayEndPrice)
+            todayMaxPriceColor = highOrLow(todayMaxPrice, yersterdayEndPrice)
+            todayMinPriceColor = highOrLow(todayMinPrice, yersterdayEndPrice)
+
+            print '%s%s%s %s%4s%s %s%10.2f%s %s%10.2f%s %s%10.2f%s %s%10.2f%s %s%10.2f   %s%s' % \
+                (bcolors.WHITE, code, bcolors.ENDC,  
+                 bcolors.WHITE, name, bcolors.ENDC,  
+                 bcolors.WHITE, yersterdayEndPrice, bcolors.ENDC,  
+                 todayBeginPriceColor, todayBeginPrice, bcolors.ENDC, 
+                 todayMaxPriceColor, todayMaxPrice, bcolors.ENDC, 
+                 todayBeginPriceColor, todayMinPrice, bcolors.ENDC, 
+                 currentPriceColor, currentPrice, per, bcolors.ENDC)
             
             
-            
+if __name__ == '__main__':
+    while True:
+        sysstr = platform.system()
+        if sysstr == 'Darwin':
+            i = os.system('clear')
+        elif sysstr == 'Windows':
+            i = os.system('cls')
+        elif sysstr == 'Linux':
+            i = os.system('clear')
+        print bcolors.YELLOW, getTime(), bcolors.ENDC
+        printStock()
+        time.sleep(5)            
